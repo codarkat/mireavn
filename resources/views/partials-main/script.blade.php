ece63e84636ae24b39bce27d7b200cde905b8bff
ledhcg <dinhcuong.firewin99@gmail.com>
2021-11-01 11:03:11 +0300

    Update project mireavn.ru

--- resources/views/partials-main/script.blade.php
+++ resources/views/partials-main/script.blade.php
@@ -1,4 +1,7 @@
-<script src="{{asset('public/admin/vendor/global/global.min.js')}}"></script>
+<script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
 <!-- Vendor scrits: js libraries and plugins-->
 <script src="{{asset('public/main/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('public/main/vendor/simplebar/dist/simplebar.min.js')}}"></script>
