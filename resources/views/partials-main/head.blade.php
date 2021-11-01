75ac3ff4e8136f12238d0b14888deb114f33dfec
ledhcg <dinhcuong.firewin99@gmail.com>
2021-11-01 23:19:13 +0300

    Update

--- resources/views/partials-main/head.blade.php
+++ resources/views/partials-main/head.blade.php
@@ -2,8 +2,7 @@
     <meta charset="utf-8">
     @yield('title')
     <!-- SEO Meta Tags-->
-    <meta name="description" content="Cartzilla - Bootstrap E-commerce Template">
-    <meta name="keywords" content="bootstrap, shop, e-commerce, market, modern, responsive,  business, mobile, bootstrap, html5, css3, js, gallery, slider, touch, creative, clean">
+    <meta name="description" content="MIREA">
     <meta name="author" content="IKBO SW">
     <!-- Viewport-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
